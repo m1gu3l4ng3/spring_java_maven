c9eadf98da1eb45b1917a7ef2b03e490c8ef2e4f
Miguel Ángel García Flores <miguelangelgarciafloresti@gmail.com>
2023-09-22 17:00:45 -0600

    feat: Add struct directories

---
+++ src/main/java/com/miguel/consumes/services/ConsumesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.consumes.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumesServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumesServicesApplication.class, args);
+	}
+
+}
